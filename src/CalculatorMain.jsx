821d75bd353af6d37224f61a3dc05eafadb3008e
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-05 15:34:28 +0200

    Added clear function and onClick handlers to calculator buttons.

--- src/CalculatorMain.jsx
+++ src/CalculatorMain.jsx
@@ -12,28 +12,34 @@ function CalculatorMain() {
     setInputValue(answer);
   }
 
+  function clear() {
+    setInputValue("");
+  }
+
   return (
     <>
       <form name="calc" className="calculator">
         <input type="text" className="value" value={inputValue} />
-        <span className="num clear">C</span>
-        <span>/</span>
-        <span>*</span>
-        <span>9</span>
-        <span>8</span>
-        <span>7</span>
-        <span>6</span>
-        <span>-</span>
-        <span>5</span>
-        <span>4</span>
-        <span>3</span>
-        <span className="plus">+</span>
-        <span>2</span>
-        <span>1</span>
-        <span>0</span>
-        <span>00</span>
-        <span>.</span>
-        <span className="equal">=</span>
+        <span className="num clear" onClick={() => clear()}>
+          C
+        </span>
+        <span onClick={}>/</span>
+        <span onClick={}>*</span>
+        <span onClick={}>9</span>
+        <span onClick={}>8</span>
+        <span onClick={}>7</span>
+        <span onClick={}>6</span>
+        <span onClick={}>-</span>
+        <span onClick={}>5</span>
+        <span onClick={}>4</span>
+        <span onClick={}>3</span>
+        <span className="plus" onClick={}>+</span>
+        <span onClick={}>2</span>
+        <span onClick={}>1</span>
+        <span onClick={}>0</span>
+        <span onClick={}>00</span>
+        <span onClick={}>.</span>
+        <span className="equal" onClick={}>=</span>
       </form>
     </>
   );
