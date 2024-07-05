4f390a1122b0f1bc465ad642a1228d1be6ebac0a
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-05 11:00:57 +0200

    Rearranged and added some number buttons in the calculator layout.

--- src/CalculatorMain.jsx
+++ src/CalculatorMain.jsx
@@ -10,17 +10,17 @@ function CalculatorMain() {
         <span className="num clear">C</span>
         <span>/</span>
         <span>*</span>
-        <span>7</span>
-        <span>8</span>
         <span>9</span>
-        <span>-</span>
-        <span>4</span>
-        <span>5</span>
+        <span>8</span>
+        <span>7</span>
         <span>6</span>
-        <span>+</span>
-        <span>1</span>
-        <span>2</span>
+        <span>-</span>
+        <span>5</span>
+        <span>4</span>
         <span>3</span>
+        <span>+</span>
+        <span>2</span>
+        <span>1</span>
         <span>0</span>
         <span>00</span>
         <span>.</span>
