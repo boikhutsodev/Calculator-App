8c6fac89b6ed1baf39a2a4bec4001e2f2da37fa1
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-05 15:38:22 +0200

    Added onClick event handlers to calculator buttons.

--- src/CalculatorMain.jsx
+++ src/CalculatorMain.jsx
@@ -23,23 +23,27 @@ function CalculatorMain() {
         <span className="num clear" onClick={() => clear()}>
           C
         </span>
-        <span onClick={}>/</span>
-        <span onClick={}>*</span>
-        <span onClick={}>9</span>
-        <span onClick={}>8</span>
-        <span onClick={}>7</span>
-        <span onClick={}>6</span>
-        <span onClick={}>-</span>
-        <span onClick={}>5</span>
-        <span onClick={}>4</span>
-        <span onClick={}>3</span>
-        <span className="plus" onClick={}>+</span>
-        <span onClick={}>2</span>
-        <span onClick={}>1</span>
-        <span onClick={}>0</span>
-        <span onClick={}>00</span>
-        <span onClick={}>.</span>
-        <span className="equal" onClick={}>=</span>
+        <span onClick={() => display("/")}>/</span>
+        <span onClick={() => display("*")}>*</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("6")}>6</span>
+        <span onClick={() => display("-")}>-</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("3")}>3</span>
+        <span className="plus" onClick={() => display("+")}>
+          +
+        </span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("0")}>0</span>
+        <span onClick={() => display("0")}>00</span>
+        <span onClick={() => display(".")}>.</span>
+        <span className="equal" onClick={() => display("=")}>
+          =
+        </span>
       </form>
     </>
   );
